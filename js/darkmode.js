6c377b2a59d1b0f8a016835a848b167cee25a282
danaray00 <danaray1004@gmail.com>
2024-01-04 18:04:16 +0900

    Create darkmode.js

---
+++ js/darkmode.js
@@ -0,0 +1,7 @@
+document.getElementById('button_toggle').addEventListener("click", function() {
+    if(document.querySelector('body').classList.contains('darkmode')){
+        document.body.classList.remove("darkmode");
+    }else{
+        document.body.classList.add("darkmode");
+    }
+},false);
