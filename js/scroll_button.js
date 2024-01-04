ce78fd698d9ba0b7707411feb446d4f87156bec5
danaray00 <danaray1004@gmail.com>
2024-01-04 18:04:20 +0900

    Create scroll_button.js

---
+++ js/scroll_button.js
@@ -0,0 +1,24 @@
+var position = 0;
+var button_background = document.getElementById("button_background");
+var toggle_button = document.getElementById("button_toggle");
+
+
+/* position 변수노트 %3
+
+0
+1   %3 = 1
+          
+*/
+
+
+function ani_slide(){
+    position++;
+    if (position % 2 == 1){
+        toggle_button.classList.add("open");
+    }
+        else {
+            toggle_button.classList.remove("open");
+        }
+}
+    toggle_button.addEventListener("click", ani_slide);
+    toggle_button.addEventLister("click", ani_slide);
