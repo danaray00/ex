6653c93f1d10d6cf402c5eea222df3824097d324
danaray00 <danaray1004@gmail.com>
2024-01-08 11:59:14 +0900

    Create hamburger.js

---
+++ js/hamburger.js
@@ -0,0 +1,12 @@
+/* 햄버거 버튼 클릭 이벤트*/
+const navToggle = document.querySelector('.hamburger');
+navToggle.addEventListener('click', ()=>{
+    navMenu.classList.toggle('open');
+})
+
+/* 모바일에서 네비게이션 메뉴 클릭하면 메뉴 숨김*/
+
+const navMenu = document.querySelector('.header_scroll');
+navMenu.addEventListener('click', () =>{
+    navMenu.classList.remove('open')
+})
