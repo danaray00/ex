c22169bd180e64307cdd901271a55e609b660a5f
danaray00 <danaray1004@gmail.com>
2024-01-08 11:59:12 +0900

    Update darkmode.js

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,5 +1,5 @@
 document.getElementById('button_toggle').addEventListener("click", function() {
-    if(document.querySelector('body').classList.contains('darkmode')){
+    if(document.querySelector('body').classList.contains("darkmode")){
         document.body.classList.remove("darkmode");
     }else{
         document.body.classList.add("darkmode");
